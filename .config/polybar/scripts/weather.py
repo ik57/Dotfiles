cc2a1617840b8a9f5155764f816c537a1a775113
ik57 <10783856+ik57@users.noreply.github.com>
2021-01-01 20:15:58 +0300

    Add the weather script for the polybar

---
+++ .config/polybar/scripts/weather.py
@@ -0,0 +1,23 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import requests
+
+CITY = ""
+API_KEY = "756edce7e9d4c385ef9499a53492678c"
+UNITS = "Metric"
+UNIT_KEY = "C"
+#UNIT_KEY = "F"
+LANG = "en"
+
+try:
+    REQ = requests.get("http://api.openweathermap.org/data/2.5/weather?id={}&lang={}&appid={}&units={}".format(CITY, LANG,  API_KEY, UNITS))
+    # HTTP CODE = OK
+    if REQ.status_code == 200:
+        CURRENT = REQ.json()["weather"][0]["description"].capitalize()
+        TEMP = int(float(REQ.json()["main"]["temp"]))
+        print("{}, {} °{}".format(CURRENT, TEMP, UNIT_KEY))
+    else:
+        print("Error: BAD HTTP STATUS CODE " + str(REQ.status_code))
+except (ValueError, IOError):
+    print("Whoopsie Daisy: Errrrrrrrror!")
