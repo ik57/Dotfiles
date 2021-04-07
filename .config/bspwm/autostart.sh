50d11f2cd8aa48a25e00c9388a1803df418f77aa
ik57 <10783856+ik57@users.noreply.github.com>
2021-04-07 09:45:08 +0300

    Update the bspwm autostart

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -29,17 +29,17 @@ xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
 #conky -c $HOME/.config/bspwm/system-overview &
-#run variety &
 run nm-applet &
 run pamac-tray &
-run xfce4-power-manager &
+#run xfce4-power-manager &
 numlockx on &
-blueberry-tray &
+#blueberry-tray &
 picom --config $HOME/.config/bspwm/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 run volumeicon &
 nitrogen --restore &
+run clipit &
 #run caffeine &
 #run vivaldi-stable &
 #run firefox &
